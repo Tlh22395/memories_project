aaa59539acd1704ca8709c1a110fa784341cf8a6
Tareeq Hamilton <tareeqhamilton@gmail.com>
2021-04-26 18:17:26 -0400

    added components folder to client, added routes models, and controllers folders to server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,24 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", exteneded: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// https://www.mongodb.com/cloud/atlas
+const CONNECTION_URL = "mongodb+srv://Tlh22395:qeeraT23@cluster0.yddjd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server runnning on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+    
+mongoose.set('useFindAndModify', false);
