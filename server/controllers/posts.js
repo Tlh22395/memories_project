c81b6800e7986fb98f56c617ebb796781ea44f30
Tareeq Hamilton <tareeqhamilton@gmail.com>
2021-05-10 15:19:27 -0400

    added update and delete functionality

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,4 @@
-import { Mongoose } from 'mongoose';
+import mongoose  from 'mongoose';
 import PostMessage from '../models/postMessage.js'
 
 
@@ -30,9 +30,20 @@ export const updatePost = async (req,res) => {
     const { id: _id } =req.params;
     const post = req.body
 
-    if(!mongoose.Types.ObjdctId.isValid(_id)) return res.status(404).send("No post with that id");
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No post with that id");
     
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id,{ ...post, _id}, { new: true });
 
     res.json(updatedPost);
+}
+
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: 'Post deleted' });
 }
