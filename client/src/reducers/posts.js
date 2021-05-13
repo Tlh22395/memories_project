10c6493635b49fb1d07f41c9f808badebd2ae628
Tareeq Hamilton <tareeqhamilton@gmail.com>
2021-05-13 16:12:03 -0400

    deployed to  heroku

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,15 +1,14 @@
-
+import { FETCH_ALL, UPDATE, DELETE, CREATE } from '../constants/actiontTypes';
 
 export default (posts = [], action) =>  {
     switch (action.type) {
-        case 'DELETE':
-            return posts.filter((post) => post._id ===! action.payload);
-        case 'UPDATE':
-        case 'LIKE':
+        case DELETE:
+        return posts.filter((post) => post._id ===! action.payload);
+        case UPDATE:
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload;
-        case 'CREATE':
+        case CREATE:
             return [...posts, action.payload];
         default:
             return posts;
--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", exteneded: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,11 +15,15 @@ app.use(cors());
 
 
 app.use('/posts', postRoutes);
+
+app.get('/', (res,req) => {
+    res.send('Hello to memories API');
+});
 // https://www.mongodb.com/cloud/atlas
-const CONNECTION_URL = "mongodb+srv://Tlh22395:qeeraT23@cluster0.yddjd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+// const CONNECTION_URL = "mongodb+srv://Tlh22395:qeeraT23@cluster0.yddjd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server runnning on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
     
